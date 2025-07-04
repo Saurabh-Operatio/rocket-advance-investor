0f06a2608db4ad3c1c582f1da748ec69e70ee248
Saurabh-Operatio <saurabh@operatiomarketing.com>
2025-07-04 23:42:16 +0530

    Update ContactUs.jsx

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./ContactUs.scss";
 export default function ContactUs() {
-  return (
+   return (
     <div className="contactUs">
       <div>Contact US</div>
-      <p style={{ marginBottom: 22 }}>Ask Us Our Different Opportunities</p>
-      <p className="linkText">info@rocketadvance.ca</p>
-      <p>1-800-518-3577</p>
+      <p style={{ marginBottom: 22 }}>Get your commission advance today!</p>
+      <p className="linkText">
+        <a href="mailto:info@rocketadvance.ca">info@rocketadvance.ca</a>
+      </p>
+      <p>
+        <a href="tel:18005183577">1-800-518-3577</a>
+      </p>
     </div>
   );
 }
